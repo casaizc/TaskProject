c92478a78d4abd3b032c5043f3d9ccb3c4405886
CARLOS SAIZ CORTES <110193113+casaizc@users.noreply.github.com>
2023-12-13 22:25:06 -0500

    Add files via upload

---
+++ src/test/java/com/project/taskProject/TaskProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.taskProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
